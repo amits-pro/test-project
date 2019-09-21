9790f558cfbea7fc8926d6df1bb84f7dca6dc58a
Amit <amits.brg@gmail.com>
2019-09-21 16:05:38 +0530

    intial files

---
+++ src/main/java/secret/mission/App.java
@@ -0,0 +1,13 @@
+package secret.mission;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is my world" );
+    }
+}
