3c5b65d069a77f5106952f467aa69703d5f75915
Amit <amits.brg@gmail.com>
2019-09-21 17:08:31 +0530

    modified app

--- src/main/java/secret/mission/App.java
+++ src/main/java/secret/mission/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! This is a new world. I love it the way it is" );
+        System.out.println( "Hello World! This is a new world. I love it the way it is working for me" );
     }
 }
